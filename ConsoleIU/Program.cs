9a894e3aca43c8719c72276dba4c37a7770ba62a
salihbenli <salihbenli0@gmail.com>
2021-02-11 13:56:52 +0300

    Add project files.

---
+++ ConsoleIU/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
+
+namespace ConsoleIU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description + " " + car.ModelYear + " yılına ait günlük fiyatı " + car.DailyPrice + " TL");
+            }
+        }
+    }
+}
